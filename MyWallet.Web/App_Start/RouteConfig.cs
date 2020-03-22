d950488971558122b4ace1a063cc6993fc2423d7
= <Israel.arauj0@live.com>
2020-03-22 21:50:20 +0000

    RavenDB: route changed to work string id route

--- MyWallet.Web/App_Start/RouteConfig.cs
+++ MyWallet.Web/App_Start/RouteConfig.cs
@@ -11,7 +11,7 @@ namespace MyWallet.Web
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{controller}/{action}/{*id}",
                 defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
